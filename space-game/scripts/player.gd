f2a4c3ea44276ffc634e1c4374587224506db4c5
butteredsaltedtoast <butteredsaltedtoast@gmail.com>
2026-08-22 10:24:42 -0400

    added acceleration to max speed of 400

--- space-game/scripts/player.gd
+++ space-game/scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const MAX_SPEED = 400.0
+var speed = 0.0
 var target = Vector2.ZERO
 
 func _ready() -> void:
@@ -15,7 +16,10 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if global_position.distance_to(target) > 3.0:
 		var direction = global_position.direction_to(target)
-		velocity = direction * SPEED
+		speed = min(speed + MAX_SPEED * (delta * 2.0), MAX_SPEED)
+		velocity = direction * speed
+		print("speed: " + str(speed))
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO
+		speed = 0.0
