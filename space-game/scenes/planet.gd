d686d5a0a058de7719c76c7355e205453198926d
trexhaydor <haydor3670@gmail.com>
2026-08-22 16:13:59 -0400

    fixed startup shop menu logic

--- space-game/scenes/planet.gd
+++ space-game/scenes/planet.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 var player_in_range: bool = false
 var player_ref: Node2D = null
 
+const SHOP_SCENE: PackedScene = preload("res://scripts/shop_menu.gd")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -23,9 +25,14 @@ func _on_body_exited(body: Node2D) -> void:
 		player_ref = null
 
 func _unhandled_input(event: InputEvent) -> void:
-	if player_in_range and event is InputEventKey and event.pressed and not event.echo and event.keycode == KEY_E:
-			var shop_node = get_tree().root.find_child("ShopMenu", true, false)
-			if shop_node and shop_node.has_method("setup_shop"):
-				shop_node.visible = true
-				shop_node.setup_shop(player_ref)
+	
+	if get_tree().root.has_node("ShopMenu"):
+		return
+		
+	var shop_node = get_tree().root.find_child("ShopMenu", true, false)
+	
+	if shop_node and shop_node.has_method("setup_shop"):
+		shop_node.visible = true
+		shop_node.setup_shop(player_ref)
+		
 	
