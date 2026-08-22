b624d3cb91c2ce40f1a30bf1583eb2b6c974f73a
butteredsaltedtoast <butteredsaltedtoast@gmail.com>
2026-08-22 14:43:11 -0400

    added custom crt-affected cursor

---
+++ space-game/scripts/software_cursor.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var cursor: Sprite2D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	cursor = Sprite2D.new()
+	cursor.texture = load("res://assets/sprites/crosshair_noring_48.png")
+	cursor.scale = Vector2(0.6, 0.6)
+	cursor.z_index = 4096
+	add_child(cursor)
+
+func _process(_delta: float) -> void:
+	cursor.global_position = get_global_mouse_position()
