f67edb6ee3f5ae0bdd934a0fb312f56cb27b77e9
trexhaydor <haydor3670@gmail.com>
2026-08-22 16:18:08 -0400

    Fixed merge error

--- space-game/scripts/planet.gd
+++ space-game/scripts/planet.gd
@@ -23,14 +23,9 @@ func _on_body_exited(body: Node2D) -> void:
 		player_ref = null
 
 func _unhandled_input(event: InputEvent) -> void:
-<<<<<<< HEAD
-	if player_in_range and event is InputEventKey and event.pressed and not event.echo and event.keycode == KEY_E:
+	if player_in_range and event is InputEventKey and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 			var shop_node = get_tree().root.find_child("ShopMenu", true, false)
 			if shop_node and shop_node.has_method("setup_shop"):
 				shop_node.visible = true
 				shop_node.setup_shop(player_ref)
-	
-=======
-	if player_in_range and event.is_button_pressed(E):
 		
->>>>>>> 24ab3ba0465f754a8fc775fa689fbd26b440f324
