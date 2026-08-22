63ff6842ca9a7d9947afad6a701b2c5986aff6fc
butteredsaltedtoast <butteredsaltedtoast@gmail.com>
2026-08-22 15:37:07 -0400

    changed cursor color, added menu with placeholders

--- space-game/scripts/software_cursor.gd
+++ space-game/scripts/software_cursor.gd
@@ -3,10 +3,11 @@ extends Node2D
 var cursor: Sprite2D
 
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	cursor = Sprite2D.new()
 	cursor.texture = load("res://assets/sprites/crosshair_noring_48.png")
-	cursor.scale = Vector2(0.6, 0.6)
+	cursor.scale = Vector2(1.0, 1.0)
 	cursor.z_index = 4096
 	add_child(cursor)
 
