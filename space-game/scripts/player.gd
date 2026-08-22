5ca1611c7c475372f9e6718be0f584f27211a3bb
butteredsaltedtoast <butteredsaltedtoast@gmail.com>
2026-08-22 10:33:21 -0400

    fix conflict error

--- space-game/scripts/player.gd
+++ space-game/scripts/player.gd
@@ -16,15 +16,10 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if global_position.distance_to(target) > 3.0:
 		var direction = global_position.direction_to(target)
-<<<<<<< HEAD
-		velocity = direction * SPEED
-		
-		rotation = lerp_angle(rotation, atan2(-velocity.y, -velocity.x), delta)
-=======
+
 		speed = min(speed + MAX_SPEED * (delta * 2.0), MAX_SPEED)
 		velocity = direction * speed
 		print("speed: " + str(speed))
->>>>>>> f2a4c3ea44276ffc634e1c4374587224506db4c5
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO
