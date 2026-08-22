d579fa744e0d9687a9f445672ddfb96ef20e6d24
butteredsaltedtoast <butteredsaltedtoast@gmail.com>
2026-08-22 14:17:30 -0400

    added combat logic

---
+++ space-game/scripts/systems/enemy_data.gd
@@ -0,0 +1,20 @@
+class_name EnemyData
+extends RefCounted
+
+const ENEMIES := {
+	"scout": {
+		"name": "Scout",
+		"health": 30.0,
+		"weak_points": 2,
+	},
+	"raider": {
+		"name": "Raider",
+		"health": 60.0,
+		"weak_points": 3,
+	},
+	"cruiser": {
+		"name": "Warship",
+		"health": 120.0,
+		"weak_points": 4,
+	},
+}
