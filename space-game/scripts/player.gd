e265c442532a5586c9a467ced8207adfd4b08604
trexhaydor <haydor3670@gmail.com>
2026-08-22 11:41:00 -0400

    Revert "Accel and Deaccel for ship"
    This reverts commit 6e6bb58e55dd273bf6af5f97b986c823a4afb4f1.

--- space-game/scripts/player.gd
+++ space-game/scripts/player.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 const MAX_SPEED = 400.0
 const TURN_SPEED = 6.0
-const DECEL_RATE = 300.0
-const ACCEL_DUR = 1.5 #1.5 seconds to reach max speed
-var accel_time = 0.0
 var speed = 0.0
 var target = Vector2.ZERO
 
@@ -19,23 +16,16 @@ func _physics_process(delta: float) -> void:
 	
 	if dist > 3.0:
 		var direction = global_position.direction_to(target)
-		var braking_distance = (speed * speed) / (2.0 * DECEL_RATE)
+		speed = min(speed + MAX_SPEED * (delta * 2.0), MAX_SPEED)
 		
-		if dist > braking_distance:
-			accel_time = min(accel_time + delta, ACCEL_DUR)
-			var t = accel_time / ACCEL_DUR
-			speed = MAX_SPEED * (t * t)
-		else: 
-			speed = max(speed - DECEL_RATE * delta, 0.0) 
-			accel_time = sqrt(speed / MAX_SPEED) * ACCEL_DUR
+		$AnimatedSprite2D.play("moving")
 			
 		var step = min(speed * delta, dist)
+		
 		velocity = direction * (step / delta)
 		rotation = lerp_angle(rotation, velocity.angle() + PI/2, TURN_SPEED * delta)
-		$AnimatedSprite2D.play("moving")
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO
 		$AnimatedSprite2D.play("idle")
 		speed = 0.0
-		accel_time = 0.0
