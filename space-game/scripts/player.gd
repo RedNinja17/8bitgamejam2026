45fdab6758e32459f76c69bd7f889f2e815d98da
trexhaydor <haydor3670@gmail.com>
2026-08-22 11:58:43 -0400

    Revised decel logic

--- space-game/scripts/player.gd
+++ space-game/scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 const MAX_SPEED = 400.0
+const ACEL_DUR = 1.5 #s
+const DECEL_RATE = 300.0
+var accel_time = 0;
 const TURN_SPEED = 6.0
 var speed = 0.0
 var target = Vector2.ZERO
@@ -16,16 +19,24 @@ func _physics_process(delta: float) -> void:
 	
 	if dist > 3.0:
 		var direction = global_position.direction_to(target)
-		speed = min(speed + MAX_SPEED * (delta * 2.0), MAX_SPEED)
+		var braking_dist = (speed * speed)/ (2 * DECEL_RATE)
 		
-		$AnimatedSprite2D.play("moving")
+		if dist > braking_dist:
+			accel_time = min(accel_time + delta, ACEL_DUR)
+			var t = accel_time / ACEL_DUR
+			speed = MAX_SPEED * (t * t)
+		else:
+			speed = max(speed - DECEL_RATE * delta, 0.0)
+			accel_time = sqrt(speed / MAX_SPEED) * ACEL_DUR
 			
 		var step = min(speed * delta, dist)
 		
 		velocity = direction * (step / delta)
 		rotation = lerp_angle(rotation, velocity.angle() + PI/2, TURN_SPEED * delta)
+		$AnimatedSprite2D.play("moving")
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO
 		$AnimatedSprite2D.play("idle")
 		speed = 0.0
+		accel_time = 0.0
